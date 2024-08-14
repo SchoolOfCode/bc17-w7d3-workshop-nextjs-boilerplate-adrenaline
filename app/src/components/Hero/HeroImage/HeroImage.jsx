db66850c0e8a549c1fa1b60e953b577d6aa0c967
habib <habibali15@hotmail.com>
2024-08-14 16:02:41 +0100

    attempting to cunderstand {children}

--- app/src/components/Hero/HeroImage/HeroImage.jsx
+++ app/src/components/Hero/HeroImage/HeroImage.jsx
@@ -1,11 +1,15 @@
 
 import styles from './HeroImage.module.css'
+import Image from 'next/image'
+
 const HeroImage = () => {
     return (
-        <img
+        <Image
         src="/images/hero-desktop.png"
         alt="Cozy living room with a fireplace"
         className={styles.heroImage}
+        width={200}
+        height={200}
       />
     )
 }
