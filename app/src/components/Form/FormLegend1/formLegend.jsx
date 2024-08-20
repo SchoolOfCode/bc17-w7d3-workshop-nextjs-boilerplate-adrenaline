af8183af9acdb1a6dcd14f058ac54483ace39623
peposlawa <peposlawa@gmail.com>
2024-08-20 14:01:21 +0100

    submit button returns data into console log

--- app/src/components/Form/FormLegend1/formLegend.jsx
+++ app/src/components/Form/FormLegend1/formLegend.jsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 
 export default function ContactForm() {
-    const [ field, setField ] = useState("");
+    const [field, setField] = useState("");
 
     function handleChange(event) {
         if (event.target.name === "name") {
@@ -17,29 +17,47 @@ export default function ContactForm() {
         if (event.target.name === "city") {
             setField(event.target.value);
         }
+        if (event.target.name === "number") {
+            setField(event.target.value);
+        }
+        if (event.target.name === "email") {
+            setField(event.target.value);
+        }
     }
-    console.log(field);
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(field);
+    }
 
 
-
-    return(
+    return (
         <>
-        <h3>Personal Information</h3>
-        <form>
-      <label htmlFor="name">Full Name</label>
-      <input type="text" id="name" name="name" value={field.name} onChange={handleChange}/>
+            <form onSubmit={handleSubmit}>
+                <fieldset>
+                    <legend>Personal Information</legend>
+                    <label htmlFor="name">Full Name</label>
+                    <input type="text" id="name" name="name" value={field.name} onChange={handleChange} />
 
-      <label htmlFor="postcode">Postcode</label>
-      <input type="text" id="postcode" name="postcode" value={field.postcode} onChange={handleChange}/>
+                    <label htmlFor="postcode">Postcode</label>
+                    <input type="text" id="postcode" name="postcode" value={field.postcode} onChange={handleChange} />
 
-      <label htmlFor="house">House/Flat Number and Street Name</label>
-      <input type="house" id="house" name="house" value={field.house} onChange={handleChange}/>
+                    <label htmlFor="house">House/Flat Number and Street Name</label>
+                    <input type="text" id="house" name="house" value={field.house} onChange={handleChange} />
 
-      <label htmlFor="city">City</label>
-      <input type="city" id="city" name="city" value={field.house} onChange={handleChange}/>
+                    <label htmlFor="city">City</label>
+                    <input type="text" id="city" name="city" value={field.city} onChange={handleChange} />
 
-      <button type="submit">Submit</button>
-    </form>
+                    <legend>Personal Information</legend>
+                    <label htmlFor="number">Phone number</label>
+                    <input type="number" id="number" name="number" value={field.number} onChange={handleChange} />
+
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" name="email" value={field.email} onChange={handleChange} />
+
+                    <button type="submit">Submit</button>
+                </fieldset>
+            </form>
         </>
     )
 }
