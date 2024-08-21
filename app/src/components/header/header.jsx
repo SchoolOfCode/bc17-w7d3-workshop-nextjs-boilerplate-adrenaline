5e9a7d3041633eaf9a690a30efaf8fb2064ebefd
peposlawa <peposlawa@gmail.com>
2024-08-21 11:52:03 +0100

    css added for the nav

--- app/src/components/header/header.jsx
+++ app/src/components/header/header.jsx
@@ -13,19 +13,21 @@ const Header = () => {
   return (
     <header className={styles.headerContainer}>
       <h1 className={styles.title}>🔥 Fireplace Palace</h1>
-      <button className ={styles.menu} onClick={handleClick}>
-      <Image
-        src="/menu-open-button.png"
-        alt="Menu"
-        className={styles.hamburgerIcon}
-        width={24}
-        height={24}
+      <button className={styles.menu} onClick={handleClick}>
+        <Image
+          src="/menu-open-button.png"
+          alt="Menu"
+          className={styles.hamburgerIcon}
+          width={24}
+          height={24}
         />
       </button>
       <nav className={menuStatus ? styles.show : styles.hide}>
         <ul>
-          <li><a href="http://localhost:3000/">Home</a></li>
-          <li><a href="http://localhost:3000/founders">Meet The Founders</a></li>
+          <li className={styles.linkStyle}>
+            <a className={styles.links} href="http://localhost:3000/">Home</a></li>
+          <li className={styles.linkStyle}>
+            <a className={styles.links} href="http://localhost:3000/founders">Meet The Founders</a></li>
         </ul>
       </nav>
     </header>
