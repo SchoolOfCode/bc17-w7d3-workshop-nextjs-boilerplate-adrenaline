788769dd4740c86c96ce0926140be9a08edea86e
habib <habibali15@hotmail.com>
2024-08-21 16:04:19 +0100

    can now type in fields

--- app/src/components/Form/ReContactForm/UseReducer.jsx
+++ app/src/components/Form/ReContactForm/UseReducer.jsx
@@ -11,27 +11,28 @@ export const initialFormState = {
     errorStatus: "",
   };
   
-  // Update the state based on which input field is being changed
-  // switch statement to handle different types of actions that can be dispatched
   export const formReducer = (state, action) => {
     switch (action.type) {
-      // case to handle input text changes in the form
+      // Handle input text changes in the form
       case "HANDLE_INPUT_TEXT":
         return {
           ...state,
           // Update the specific field that changed in the fields object
-          // [action.field] is the key in the fields object that we want to update
-          // action.payload is the value that we want to update the key with
-          [action.field]: action.payload,
+          fields: {
+            ...state.fields, // Copy the existing fields
+            [action.field]: action.payload, // Update the specific field
+          },
         };
-      // below case to handle form submission error
+        
+      // Handle form submission error
       case "HANDLE_ERROR":
         return {
           ...state,
           // Update the error message
-          error: action.payload,
+          errorStatus: action.payload,
         };
-      // default case to return the state if no action type matches which means no changes to the state
+        
+      // Default case to return the state if no action type matches
       default:
         return state;
     }
